292542c39e0340b82a4fd03243dd61a778932c5d
estebansolanog <estebansolanoguillen@gmail.com>
2023-04-12 03:07:39 +0000

    Tarea Finalizada

---
+++ src/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request 
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def todos_get():
+    # you can convert that variable into a json string like this
+    json_text = jsonify(todos)
+
+    # and then you can return it to the front end in the response body like this
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+
+    todos.append(request_body)
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    position = int(position)
+    if position >= len(todos):
+       return jsonify({"message":"índice inválido"}), 429
+    if position < 0:
+       return jsonify({"message":"índice inválido"}), 410
+    if  len(todos)==0:
+       return jsonify({"message":"índice inválido"}), 402
+
+    todos.pop(position)
+
+    json_text = jsonify(todos)
+    return json_text
+
+
+
+
+
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
